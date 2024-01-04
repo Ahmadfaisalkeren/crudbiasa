7b6778d35cf72bfe1c2d4f0de104720cbe472b51
Ahmad Faisal <maulidae.rizki@gmail.com>
2024-01-04 14:26:56 +0700

    update vite config pt.2

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-// Middleware to enable CORS
-const corsMiddleware = () => {
-  return (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', '*');
-    next();
-  };
-};
-
 export default defineConfig({
-  server: {
-    middleware: corsMiddleware(),
-  },
-  plugins: [
-    laravel({
-      input: ['resources/css/app.css', 'resources/js/app.js'],
-      refresh: true,
-    }),
-  ],
+    plugins: [
+        laravel({
+            input: [
+                'resources/css/app.css',
+                'resources/css/style.css',
+                'resources/js/app.js',
+                'resources/js/csrf.js',
+                'resources/js/ajax.js',
+            ],
+            refresh: true,
+        }),
+    ],
 });
